5dc1c16f757802aebf4adaf8d073e7facdbbc7ca
ADESSO\guth <yannick.guth@adesso.de>
2017-12-05 10:01:58 +0100

    eureka support

--- src/main/java/de/cluster/microservices/locations/MicroserviceLocationsApplication.java
+++ src/main/java/de/cluster/microservices/locations/MicroserviceLocationsApplication.java
@@ -2,8 +2,10 @@ package de.cluster.microservices.locations;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class MicroserviceLocationsApplication {
 
 	public static void main(String[] args) {
